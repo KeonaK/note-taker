b6726f7dbcbe643d690f20363e530e918800ddeb
Keona Kirby <keonakirby@gmail.com>
2021-04-14 22:00:14 -0500

    Add/server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+// Tells node that we are creating an "express" server
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// LISTENER
+// The below code effectively "starts" our server
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
