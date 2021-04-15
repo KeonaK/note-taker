66e821d1eb1364ad946d9b1678ec82f1092468a4
Keona Kirby <keonakirby@gmail.com>
2021-04-14 21:46:59 -0500

    Add/htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+//include the path module 
+// provides functionality to access and interact with the file system
+const path = require("path");
+
+module.exports = (app) => {
+
+    app.get("/notes", (req,res) => {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    //  defaults to index.html if a matching route is not found
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
