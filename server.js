07868e44e4579b5c72de58177cc7d6c28f24b4b9
Keona Kirby <keonakirby@gmail.com>
2021-04-15 19:58:28 -0500

    Update/notes in server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-
+//handles form submissions
 app.use(express.urlencoded({ extended: true }));
+//Body Parser middleware, allows us to use raw json
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -13,6 +14,4 @@ require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 //starts up the server
-app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
-});
+app.listen(PORT, () =>  console.log(`App listening on PORT: ${PORT}`));
